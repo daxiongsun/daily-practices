c25b24408b76c20e4902cec32fd7d578dd3c7984
daxiongsun <itdaxiong@163.com>
2018-11-24 16:29:25 +0800

    JVM Class Loading

---
+++ mymind/springcloud/code/microservice-provider-user-my-metadata/src/main/java/com/ngsky/microserviceproviderusermymetadata/MicroserviceProviderUserMyMetadataApplication.java
@@ -0,0 +1,14 @@
+package com.ngsky.microserviceproviderusermymetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProviderUserMyMetadataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProviderUserMyMetadataApplication.class, args);
+
+        java.lang.Class
+    }
+}
